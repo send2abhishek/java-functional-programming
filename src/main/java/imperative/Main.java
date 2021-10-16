6d4e7ca0e3cb878f7495432ef16f7f0e928e8603
Abhishek Kumar <send2abhishek@live.com>
2021-10-16 21:37:20 +0530

    add sample list

--- src/main/java/imperative/Main.java
+++ src/main/java/imperative/Main.java
@@ -1,8 +1,44 @@
 package imperative;
 
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("lets test this ");
+
+        List<Person> people = List.of(
+
+                new Person("Abhishek kumar", Gender.MALE),
+                new Person("Aryan", Gender.MALE),
+                new Person("Arti", Gender.FEMALE)
+
+        );
+
+        people.forEach(person -> {
+            System.out.println(person.toString());
+        });
+    }
+
+    static class Person {
+
+        private final String name;
+        private final Gender gender;
+
+        Person(String name, Gender gender) {
+            this.name = name;
+            this.gender = gender;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    ", gender=" + gender +
+                    '}';
+        }
+    }
+
+    enum Gender {
+        MALE, FEMALE;
     }
 }
