e1b36e6468204f6c8ec26d7eb3dee4a8fe654284
Abhishek Kumar <abhishek160.kumar@ril.com>
2021-12-12 13:00:43 +0530

    refactor: filterlist

--- src/main/java/steams/Main.java
+++ src/main/java/steams/Main.java
@@ -3,16 +3,32 @@ package steams;
 import steams.mockdata.MockData;
 import steams.model.Person;
 
+import java.util.List;
+
+
 
 public class Main {
-
     private static void  display(Person person) {
         System.out.println(person.getFirstName());
     }
 
+    private static List<Person> getData(){
+        return MockData.getData();
+    }
+
+    // get the list of data whose age is less than 30
+    private static void filterList(){
+        getData().stream().filter(person -> person.getAge()<30).limit(2).forEach(Main::display);
+    }
+
+
     public static void main(String[] args) {
 
-        // get the list of 2 people whose age is less than 30
-      MockData.getData().stream().filter(person -> person.getAge()<30).limit(2).forEach(Main::display);
+        filterList();
+
+
     }
+
+
+
 }
